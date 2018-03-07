4a614fc02ab3aa92db14d3e65be7f6bf8b178157
woojunchoi <tim.woojune1@gmail.com>
2018-03-06 22:36:48 -0800

    server setup

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+//connect to Mongoose
+mongoose.connect('mongodb://localhost/bookstore')
+const db = mongoose.connection
+
+app.get('/', (req,res) => {
+    res.send('hello world')
+})
+
+app.listen(3000);
+console.log('running on port 3000')
